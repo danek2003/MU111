af3537363af2938a43c13d1b9a99e50744bb766f
Danilenko Evgeny <Danek@MacBook-Pro-Danilenko.local>
2014-10-12 01:09:30 +1100

    ver1

--- MU111-hw2_part1/ViewController.swift
+++ MU111-hw2_part1/ViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var LoginTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var LoginViewInfo: UITextView!
 
+    @IBAction func LoginButton(sender: AnyObject) {
+    LoginViewInfo.text = "логин: \(LoginTextField.text)  Пароль:  \(PasswordTextField.text)"
+    println("ввел логин: \(self.LoginTextField.text) и пароль: \(self.PasswordTextField.text)")
+    }
+    
+    
+    @IBAction func End_enter(sender: UITextField) {
+        self.PasswordTextField.resignFirstResponder()
+        self.LoginTextField.resignFirstResponder()
+
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
