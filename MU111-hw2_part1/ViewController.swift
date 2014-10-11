7297192d52f0af402675a8b72c344084df66505d
Danilenko Evgeny <Danek@MacBook-Pro-Danilenko.local>
2014-10-11 23:58:22 +1100

    Initial Commit

---
+++ MU111-hw2_part1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MU111-hw2_part1
+//
+//  Created by Danilenko Evgeny on 11.10.14.
+//  Copyright (c) 2014 Danilenko Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
