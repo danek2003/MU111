9a76c722d7430514b1ed4db64f719f71db51f663
Danilenko Evgeny <Danek@MacBook-Pro-Danilenko.local>
2014-10-12 16:38:30 +1100

    _

--- MU111-hw2_part1/LecturesViewController.swift
+++ MU111-hw2_part1/LecturesViewController.swift
@@ -13,16 +13,22 @@ class LecturesViewController: UIViewController {
     @IBOutlet weak var PasswordTextField: UITextField!
     @IBOutlet weak var LoginViewInfo: UITextView!
 
+
     @IBAction func LoginButton(sender: AnyObject) {
-    LoginViewInfo.text = "логин: \(LoginTextField.text)  Пароль:  \(PasswordTextField.text)"
-    println("ввел логин: \(self.LoginTextField.text) и пароль: \(self.PasswordTextField.text)")
+        LoginViewInfo.text = "логин: \(LoginTextField.text)  Пароль:  \(PasswordTextField.text)"
+        println("ввел логин: \(self.LoginTextField.text) и пароль: \(self.PasswordTextField.text)")
+  
+        var myAlertView = UIAlertView()
+      //  myAlertView.title = "Info"
+        myAlertView.message = "ввел логин: \(self.LoginTextField.text) и пароль: \(self.PasswordTextField.text)"
+        myAlertView.addButtonWithTitle("ok")
+        myAlertView.show()
     }
     
     
     @IBAction func End_enter(sender: UITextField) {
         self.PasswordTextField.resignFirstResponder()
         self.LoginTextField.resignFirstResponder()
-
     }
    
     override func viewDidLoad() {
